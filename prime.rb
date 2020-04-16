79693da178bee53492141e0e662c9a998524855a
Danny Padron <danny.padron09@gmail.com>
2020-04-16 21:45:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   #a boolean whether integer is prime
   i = 2
   if integer > 1
-    range = (start..integer - 1).to_a
+    range = (i..integer - 1).to_a
     range.none? do |i|
       integer % i == 0
     end
