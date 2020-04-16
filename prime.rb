a13a5bfe6fa96ef54cd1f4e8dc9d4ceba5a38df0
Danny Padron <danny.padron09@gmail.com>
2020-04-16 21:30:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime(integer)
   #a boolean whether integer is prime
   (2..(integer[-1])). each do |i|
-    return false if integer % i == 0
+    return false if integer % i == 1
   end
   true
 end
